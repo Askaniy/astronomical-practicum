ad538f9ce904eb98a4bf1244d4fd9103004a71fb
Askaniy <44873271+Askaniy@users.noreply.github.com>
2024-02-28 11:04:19 +0300

    Выравнивание

--- Practice-1/practice1_step1.py
+++ Practice-1/practice1_step1.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from itertools import chain
 from astropy.io import fits
+from image_registration import chi2_shift
+from image_registration.fft_tools import shift
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,6 +28,9 @@ def array2img(array: np.ndarray):
     img = Image.fromarray(array_norm.clip(0, 255).astype('uint8'), mode='L')
     return img
 
+def crop(array):
+    """ Инверсия вертикальной оси и обрезка чёрных краёв """
+    return (array[::-1,:])[20:,19:]
 
 # Чтение смещений
 
@@ -37,12 +42,12 @@ for file in fits_list(folder):
         #print(repr(hdul[0]))
         header = hdul[0].header
         if header['IMAGETYP'] == 'bias':
-            data = hdul[0].data
+            data = crop(hdul[0].data)
             bias_list.append(data)
             #save_histogram(data, f'{folder}/{file.stem}.png')
 
 bias_array = np.median(np.array(bias_list), axis=0)
-save_histogram(bias_array, f'{folder}/bias_histogram.png')
+#save_histogram(bias_array, f'{folder}/bias_histogram.png')
 array2img(bias_array).save(folder/'bias.png')
 
 
@@ -54,27 +59,32 @@ for file in fits_list(folder):
     with fits.open(file) as hdul:
         header = hdul[0].header
         if header['IMAGETYP'] != 'bias':
-            data = (hdul[0].data - bias_array) / header['EXPTIME']
+            data = (crop(hdul[0].data) - bias_array) / header['EXPTIME']
             flat_field_list.append(data)
             #save_histogram(data, f'{folder}/{file.stem}.png')
 
 flat_field_array = np.median(np.array(flat_field_list), axis=0)
 flat_field_array /= flat_field_array.mean()
 flat_field_array = np.clip(flat_field_array, 0.01, None)
-save_histogram(flat_field_array, f'{folder}/flat_field_histogram.png')
+#save_histogram(flat_field_array, f'{folder}/flat_field_histogram.png')
 array2img(flat_field_array).save(folder/'flat_field.png')
 
 
 # Чтение фотографий
+
+def shifted(reference, target):
+    xoff, yoff = chi2_shift(reference, target, return_error=False, upsample_factor='auto')
+    return shift.shift2d(target, -xoff, -yoff)
 
 def band_reader(name: str):
     band_list = []
     for file in fits_list(folder/name):
         with fits.open(file) as hdul:
             header = hdul[0].header
-            data = np.clip(hdul[0].data - bias_array, 0, None) / header['EXPTIME']
+            data = (crop(hdul[0].data) - bias_array) / header['EXPTIME'] / flat_field_array
             band_list.append(data)
             #save_histogram(data, f'{folder}/{name}/{file.stem}.png')
+    band_list[1] = shifted(band_list[0], band_list[1])
     return np.mean(np.array(band_list), axis=0)
 
 bands = ('B', 'V', 'R', 'I')
@@ -82,14 +92,7 @@ band_list = []
 for band in bands:
     band_list.append(band_reader(band))
 
-photospectral_cube = np.array(band_list) / flat_field_array
-photospectral_cube = photospectral_cube[:,::-1,:] # инверсия вертикальной оси
-photospectral_cube = photospectral_cube[:,20:,19:] # обрезка чёрных краёв
+photospectral_cube = np.array(band_list)
 
 for i in range(len(bands)):
-    img = array2img(photospectral_cube[i])
-    img.save(f'{folder}/band_{i}_{bands[i]}.png')
-
-
-# Выравнивание
-
+    array2img(photospectral_cube[i]).save(f'{folder}/band_{i}_{bands[i]}.png')
